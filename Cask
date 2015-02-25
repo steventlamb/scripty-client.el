46f060f410637184e49936ffc9af5ebcde16b737
Steve Lamb <slamb@azavea.com>
2015-02-24 19:50:46 -0500

    Initital Commit

---
+++ Cask
@@ -0,0 +1,8 @@
+(source gnu)
+(source melpa)
+
+(package "scripty-client" "0.1.0" "Run scripty from emacs")
+(files "README.md" "scripty.el")      
+
+(depends-on "dash")
+(depends-on "s")
---
+++ scripty.el
@@ -0,0 +1,23 @@
+(require 'dash)
+(require 's)
+
+(defvar scripty-executable "scripty")
+
+(defun scripty-get-choices ()
+  (interactive)
+  (let* ((list-command (concat scripty-executable " -l"))
+         (cmd-results (shell-command-to-string list-command))
+         (lines (if (equal "" cmd-results) '() (s-split "\n" cmd-results))))
+    (-map 's-trim lines)))
+
+(defun scripty (arg)
+  (interactive 
+   (let ((choices (scripty-get-choices)))
+     (if (null choices)
+         '("")
+       (list (ido-completing-read "command: " (scripty-get-choices))))))
+  (if (equal arg "")
+      (message "No scripty dir found")
+    (let ((additional-args (read-string "args: ")))
+      (async-shell-command (concat scripty-executable " " arg " " additional-args)
+                           (concat "*scripty-" arg "*")))))

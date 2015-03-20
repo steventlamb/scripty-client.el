523b49af41fff81b5ac48f1d4b1ef5ef744cc70a
Steve Lamb <slamb@azavea.com>
2015-03-20 18:43:51 -0400

    Add option to prompt for buffer name
    fixes #3

--- scripty-client.el
+++ scripty-client.el
@@ -11,6 +11,9 @@
 (defvar scripty-last-args nil "do not hand-edit")
 (put 'scripty-last-args 'risky-local-variable t)
 
+(defcustom scripty-confirm-buffer-name nil
+  "Prompt the user to edit buffer names when set to `t`")
+
 (defun scripty-get-choices ()
   (interactive)
   (let* ((list-command (concat scripty-executable " -l"))
@@ -26,6 +29,28 @@
               (concat "command (default: \"" scripty-last-script "\"): ") choices nil t)))
         (if (s-blank? completion) scripty-last-script completion)))))
 
+(defun scripty-get-buffer-name (command)
+  "given the name of a command, build a suggested buffer name.
+
+The default syntax is '*scripty-<command>*', but we must first look
+for existing buffers with that name and add an increasing unique id,
+for example, '*scripty-<command>*<1>'
+
+Finally, prompt to confirm the choice of name if
+`scripty-confirm-buffer-name` is `t`.
+"
+  (let* ((base-name (concat "*scripty-" command "*"))
+         (suffix "")
+         (id 1)
+         (name base-name))
+    (while (get-buffer name)
+      (setq id (+ 1 id))
+      (setq suffix (concat "<" (number-to-string id) ">"))
+      (setq name (concat base-name suffix)))
+    (if scripty-confirm-buffer-name
+        (setq name (read-string "buffer name: " name)))
+    name))
+
 (defun scripty ()
   ;; populate command
   (interactive)
@@ -35,10 +60,10 @@
     ;; process command, prompt for arg, execute, save last values
     (if (null command)
         (message "No scripty dir found")
-      (let ((args (read-string "args: " (lax-plist-get scripty-last-args command))))
+      (let ((args (read-string "args: " (lax-plist-get scripty-last-args command)))
+            (buffer-name (scripty-get-buffer-name command)))
         (setq scripty-last-script command)
         (setq scripty-last-args (lax-plist-put scripty-last-args command args))
-        (async-shell-command (concat scripty-executable " " command " " args)
-                             (concat "*scripty-" command "*"))))))
+        (async-shell-command (concat scripty-executable " " command " " args) buffer-name)))))
 
 (provide 'scripty-client)
